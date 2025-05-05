8ab0dc0d1d8cdd0a00769109e082ae5c749f3202
James Minton <james7minton@gmail.com>
2025-05-05 21:56:06 +0100

    implemented redux

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,3 +1,25 @@
+import { decrement, increment } from "./counderReducer";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
+
 export default function ContactPage() {
-  return <div>ContactPage</div>;
+  const {data} = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+  return (
+    <>
+      <Typography variant="h2">Contact Page</Typography>
+      <Typography variant="body1">Data is: {data}</Typography>
+      <ButtonGroup>
+        <Button onClick={() => dispatch(decrement(1))} color="error">
+          Decrement
+        </Button>
+        <Button onClick={() => dispatch(increment(1))} color="secondary">
+          Increment
+        </Button>
+        <Button onClick={() => dispatch(increment(5))} color="primary">
+          Increment by 5
+        </Button>
+      </ButtonGroup>
+    </>
+  );
 }
